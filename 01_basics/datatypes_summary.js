814ce66cb33726f7f6f1dcb823c5a32ef70798c7
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-01-27 16:19:03 +0000

    stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -5,10 +5,10 @@
 
 const id = Symbol('213')
 const anotherId = Symbol('213')
-console.log(id)
-console.log(anotherId)
+// console.log(id)
+// console.log(anotherId)
 
-console.log(id === anotherId)
+// console.log(id === anotherId)
 
 const bigNumber = 45555555555555555n 
 
@@ -25,4 +25,28 @@ const myFunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
+
+
+
+//**************************************************************************************
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let name = "Prince";
+let anotherName = name;
+anotherName = "PanditJi"
+console.log(anotherName);
+console.log(name); // it gives copy of variable's value
+
+let user1 ={
+    email: "pince@gmail.com",
+    upi: "user@ypl",
+}
+
+let user2 = user1;
+
+user2.email = "princeSharma@gmail.com"; // it gives reference of  variable's value
+
+console.log(user1.email)
+console.log(user2.email)
