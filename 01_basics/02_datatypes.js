63cabf739959255d0a348c6ccae69f537068ea76
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-01-27 14:19:48 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+//datatypes on javascript
+//Primitive Datatype
+//string, number, bigInt, boolean, undefined, NaN(Not A Number), Null
+
+//Non-Primitive
+//array, object
+
+"use strict"; //treat all JS code as a newer version
+
+//window.alert(1+5); We are using node.js not browser
+
+let name = "prince";
+let age = 21;
+let isLoggedIn = false; 
+let empty = null;
+let state;
+
+console.log(typeof name); //string
+console.log(typeof age); //number
+console.log(typeof isLoggedIn); //boolean
+console.log(typeof empty); //object
+console.log(typeof state); //undefined
