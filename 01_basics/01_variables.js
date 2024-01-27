b03318757347fd06d8ab85c0ee81307e27bf7fdd
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-01-27 13:59:38 +0000

    container congif done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+// console.log("Hello Prince");
+const accountId = 144553;
+let accountEmail = "princesharma@softlabx.com";
+var accountPass = 456456;
+accountCity = "Delhi";
+let accountState;
+
+// accountId = 5; not allowed
+console.log(accountId);
+
+accountEmail = "prince@gmail.com";
+accountPass = 789456;
+accountCity = "uttar pardesh"
+
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]);
+
+//let has block scope
+//var has function scope
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
