879e10b25f88325a6914d0d1768e9c70d5c39fb6
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-01-27 14:59:22 +0000

    data type & arthemetic conversion operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33abc";
-console.log(typeof score);      //string
+// console.log(typeof score);      //string
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber); //number
-console.log(valueInNumber); //NaN (Not A Number)
+// console.log(typeof valueInNumber); //number
+// console.log(valueInNumber); //NaN (Not A Number)
 
 /*
 '33' => 33
@@ -15,11 +15,42 @@ true => 1, false=> 2
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true, 0=> false
 // "" => false, "Prince" => true
 
 let num = 33;
 let numIntoString = String(num);
-console.log(typeof numIntoString); //string
+// console.log(typeof numIntoString); //string
+
+
+// **************************** Operations ********************************
+
+let value = 3
+let negValue = -value;
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello";
+let str2 = "Prince";
+let str3 = str1 + " " + str2;
+console.log(str3)
+
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
