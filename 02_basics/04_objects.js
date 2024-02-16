04a4be233e7e83409fe041d11371d7b85917feed
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-02-16 17:36:18 +0000

    object properties

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,57 @@
+// singleton
+
+const tinderUser = new Object() 
+tinderUser.id = "123abc"
+tinderUser.name = "Prince"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser)
+
+const regularUser = {
+    email: "AlphaPrince@gmail.com",
+    fullname: {
+        userfullname:{
+            firstname: "Prince",
+            lastname: "Sharma"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname) //? is usde to protect your object
+
+const obj1 = {1:'a', 2:'b'}
+const obj2 = {3:'a', 4:'b'}
+const obj4 = {5:'a', 6:'b'}
+
+// const obj3 = obj1 + obj2
+// const obj3 = Object.assign({}, obj1, obj2, obj4); // create new object
+
+const obj3 = {...obj1, ...obj2, ...obj4}
+
+console.log(obj3)
+
+const users = [
+    {
+        id: 1,
+        email: "alphaprince0093@gmail.com"
+    },
+    {
+        id: 1,
+        email: "alphaprince93@gmail.com"
+    },
+    {
+        id: 1,
+        email: "alphaprince0093@gmail.com"
+    }
+]
+
+// console.log(users[1])
+
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser))
+console.log(Object.values(tinderUser))
+console.log(Object.entries(tinderUser))
+// console.log(Object.entries(tinderUser))
+
+console.log(tinderUser.hasOwnProperty("isLoggedIn"))
