a9bfaa606835a71efd4a2a897ab23121576cc974
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-02-18 17:39:10 +0000

    basics off function

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -35,3 +35,32 @@ function loginUserMessage(userName = "anu"){
 }
 // console.log(loginUserMessage("prince"))
 console.log(loginUserMessage("PRince"))
+
+function calculateCartPrice(...num1){
+    return num1
+}
+// console.log(calculateCartPrice(200, 400, 500))
+
+const user = {
+    userName: "prince",
+    price: 199
+}
+
+function handleObject(anyObj){
+    console.log(`Username is ${anyObj.userName} and Price is ${anyObj.price}`)
+}
+
+// handleObject(user);
+handleObject({
+    userName: "Anu",
+    price: 499
+});
+
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1];
+}
+// console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200, 400, 1000]))
