ddfe17b5e8ba60b9b8c5256e36419833bf6dd49e
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-02-18 18:10:31 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,9 +5,46 @@ if(true){
     let a = 10;
     const b = 20;
     // var c = 30;
-    console.log("INNER: ", a)
+    // console.log("INNER: ", a)
 }
 
-console.log(a)
+// console.log(a)
 // console.log(b)
 // console.log(c)
+
+function one(){
+    const Username = "prince";
+
+    function two(){
+        const website = "youtube"
+        console.log(Username)
+    }
+    // console.log(website)
+    two() // in nested functions child can access the parent variables
+}
+
+// one()
+
+if(true){
+    const Username = "Prince"
+    if(Username === "Prince"){
+        const website = "prince.com"
+        console.log(Username + " " + website)
+    }
+    // console.log(website)
+}
+// console.log(Username)
+
+
+// ****************************** interesting *************************************
+
+function addOne(value){
+    return value + 1
+}
+addOne(5)
+
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
