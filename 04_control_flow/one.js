5197ae4d1a2a92bff603fcca16c60c820bb5d749
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-02-20 17:53:32 +0000

    basics of if else statements

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,8 +1,45 @@
 //if 
-const isUserloggedIn = true
-if(isUserloggedIn){
+// const isUserloggedIn = true
 
-}
+// const temperature = 41
+
+// if(temperature < 50){
+// console.log("less than 50")
+// }
+// else{
+//     console.log("temperature is greater than 50");
+// }
+    
 
 
 // <, >, <=, >=, ==, !=, ===
+
+// const score = 200
+// if(score > 100){
+//     const power = "fly";
+//     console.log(`user power: ${power}`);
+// }
+
+// const balance = 1000
+
+// if(balance > 500) console.log("test")  //implicit scope
+
+// if(balance < 500){
+//     console.log("less than 500")
+// } else if(balance < 750)    {
+//     console.log("less than 750")
+// } else if(balance < 900){
+    
+//     console.log("less than 900")
+// }
+// else{
+//     console.log("less than 1200")
+// }
+
+
+const isUserloggedIn = true
+const debitCard = true
+
+if(isUserloggedIn && debitCard){
+    console.log("Allow to buy courses");
+}
