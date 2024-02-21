d09ad926131b850f7edcdc067e12e172b65e1525
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-02-21 13:58:06 +0000

    swtch and case statements

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,29 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January")
+        break;
+    case "feb":
+        console.log("feb")
+        break;
+    case "march":
+        console.log("March")
+        break;    //break is used to break control flow
+    case "april":
+        console.log("April")
+        break;
+
+    default:
+        console.log("this is default")
+        break;
+}
