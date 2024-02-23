695c0996f01cef42f98a1014b59ad8c0a8bd430c
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-02-23 16:59:41 +0000

    basics of reduce in js

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,7 +1,37 @@
 const myNums = [1, 2, 3]
 
-const newNums = myNums.reduce( (acc, cvalue)=>{
-    return acc + cvalue
-}, 0) // we can define accumulator's value here
+// const newNums = myNums.reduce( (acc, cvalue)=>{  // reduce automatically passes curvalue into accumulator
+//     console.log(`acc: ${acc} and curvalue: ${cvalue}`)
+//     return acc + cvalue
+// }, 0) // we can define accumulator's value here
 
-console.log(newNums)
+
+
+// console.log(newNums)
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 4999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+    {
+        itemName: "java course",
+        price: 3999
+    },
+]
+
+const priceToPay = shoppingCart.reduce( (acc, item) => (acc + item.price), 0)
+console.log(priceToPay)
