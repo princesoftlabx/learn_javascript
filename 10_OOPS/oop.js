d9f1f83fb0dde83f335498abcbe9e986718c7bce
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-03-23 18:24:16 +0000

    basics of this, constructor function and new keyword

---
+++ 10_OOPS/oop.js
@@ -0,0 +1,36 @@
+const user = {
+    username: "Prince",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        // console.log("Got user details from database")
+        // console.log(`Username: ${this.username}`)
+        console.log(this)
+    }
+}
+
+
+console.log(user.username)
+// console.log(user.getUserDetails())
+// console.log(this)
+// constructor is reference of a user
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username
+    this.loginCount = loginCount
+    this.isLoggedIn = isLoggedIn
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`)
+    }
+
+    return this  // it is defined implictly
+}
+
+const userOne = new User("Prince", 12, true)
+const userTwo = new User("Anshul", 18, false)  // new will create empty object or new instance
+// constructor function calls because of new keyword and get packed all arguments and give
+// this keyword or argument will injected
+// we can print that
+console.log(userOne.constructor)
+console.log(userTwo)
