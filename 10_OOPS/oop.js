5a531f1731500d1cd3e592fd28c4e032153b9005
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-04-04 08:02:28 +0000

    basics of new keyword, constructor function and instance of method

--- 10_OOPS/oop.js
+++ 10_OOPS/oop.js
@@ -28,9 +28,18 @@ function User(username, loginCount, isLoggedIn){
 }
 
 const userOne = new User("Prince", 12, true)
-const userTwo = new User("Anshul", 18, false)  // new will create empty object or new instance
+const userTwo = new User("Anshul", 18, false)
+const d = [1,2,3]
+// Execution of new or constructor function in four steps  
+// new will create empty object or new instance
 // constructor function calls because of new keyword and get packed all arguments and give
 // this keyword or argument will injected
 // we can print that
 console.log(userOne.constructor)
+// constructor function is reference of user
 console.log(userTwo)
+console.log("hi Jahanvi")
+ 
+console.log(userTwo instanceof User)
+console.log(userOne instanceof Object)
+console.log(d instanceof User)
