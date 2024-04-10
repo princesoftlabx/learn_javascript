a6c3836d06e2b9e0f5f21e1ba290c8d0d29b326b
Prince Sharma <145842365+princesoftlabx@users.noreply.github.com>
2024-04-10 13:04:49 +0000

    basics of prototypal inheritance

--- 10_OOPS/prototype.js
+++ 10_OOPS/prototype.js
@@ -1,2 +1,67 @@
-let myName = "Prince     ";
-console.log(myName.length);
+// let myName = "Prince     ";
+// console.log(myName.truelength);
+
+
+let myHeroes = ["thor", "spiderman", "ironman"]
+let heroPower = {
+    thor: "hammer",
+    spiderman: "sling",
+    ironman: "garvis",
+
+    getSpiderPower: function () {
+        console.log(`Spidy power is ${this.spiderman}`)
+    }
+}
+
+Object.prototype.prince = function(){
+    console.log(`Prince is present in all object`)
+}
+
+Array.prototype.heyPrince = function(){
+    console.log(`Prince says hello`);
+}
+
+
+heroPower.prince()
+myHeroes.prince()
+// heroPower.heyPrince()
+myHeroes.heyPrince()
+
+
+// inheritance
+
+const user= {
+    name: "Prince",
+    email: "alphaprince0093@gmail.com"
+}
+
+const teacher = {
+    makeVideo: true
+}
+
+const teachingSupport = {
+    isAvailable: false
+}
+
+const TASupport = {
+    makeAssignment: 'JS assignment',
+    fullTime: true,
+    __proto__: teachingSupport
+}
+
+teacher.__proto__ = user
+
+//modernSyntax
+
+Object.setPrototypeOf(user, TASupport)
+
+let anotherName = "PrinceSharma         "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`);
+    console.log(`${this.trim().length}`);
+}
+
+anotherName.trueLength()
+"PanditJi".trueLength()
+"Graduate".trueLength()
